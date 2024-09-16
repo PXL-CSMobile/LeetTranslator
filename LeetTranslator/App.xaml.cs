d60c202eb957a491022e7676c3e395cb5e47f2ed
Wim Bervoets <wim.bervoets@pxl.be>
2024-09-16 20:34:59 +0200

    Add project files.

---
+++ LeetTranslator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetTranslator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LeetTranslator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetTranslator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
